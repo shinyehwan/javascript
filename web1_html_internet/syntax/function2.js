211b024fcc21fd2fbfe85ce91fc1b344faea9b25
shinyehwan <jghisjl007@naver.com>
2022-01-09 19:37:56 +0900

    2022.01.09 [NOJ_220109] DAY7

---
+++ web1_html_internet/syntax/function2.js
@@ -0,0 +1,16 @@
+console.log(Math.round(1.6)); // 2
+console.log(Math.round(1.4)); // 1
+
+
+function sum(first, second){ // parameter
+    console.log('a');
+    return first+second; // return을 만나면 즉시 그 함수를 종료시킨다.
+    console.log('b');
+}
+console.log(sum(2,4)); // argument
+
+
+
+// console.log(Math.round(1.6));
+// filewrite('result.txt', Math.round(1.6));
+// email('jghisjl007@naver.com',Math.round(1.6));
