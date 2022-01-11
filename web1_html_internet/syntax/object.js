69f5b5433edd70778e07a4f1426095cea5f3c3ad
shinyehwan <jghisjl007@naver.com>
2022-01-11 21:21:13 +0900

    2021.01.11

---
+++ web1_html_internet/syntax/object.js
@@ -0,0 +1,16 @@
+var members = ['shinyehwan', 'k8805', 'jghisjl007'];
+console.log(members[1]); // k8805
+
+var i = 0;
+while(i<members.length){
+    console.log(members[i]);
+    i = i+1;
+
+}
+
+var roles={
+    'programmer' : 'shinyehwan',
+    'designer' : 'k8805',
+    'manager' : 'jghisjl007'
+}
+console.log(roles.designer);
