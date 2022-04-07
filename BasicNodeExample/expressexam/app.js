6d1ebdd852329a48bd0541542c34dac85b266873
shinyehwan <jghisjl007@naver.com>
2022-04-07 16:40:45 +0900

    2022.4.7 기임실 5주차

---
+++ BasicNodeExample/expressexam/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res){
+    res.send('hello world');
+});
+
+app.get('/hello', function(req,res){
+    res.send('Dankook Univ');
+});
+
+app.get('/html', function(req,res){
+    res.sendfile('intro.html');
+});
+
+
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000!');
+});
