3eb5df69fffa270da27cbf0ef9b2ea6f64a3577f
shinyehwan <jghisjl007@naver.com>
2022-04-15 17:32:09 +0900

    2022.4.15

--- BasicNodeExample/expressexam/app.js
+++ BasicNodeExample/expressexam/app.js
@@ -21,6 +21,20 @@ app.get('/hrtag', function(req,res){
     res.sendfile('intro3.html');
 })
 
+app.get('/tagtext',function(req,res){
+    res.sendfile('intro4.html');
+})
+
+app.get('/hyperweb',function(req,res){
+    res.sendfile('intro5.html');
+})
+app.get('/hyperweb2',function(req,res){
+    res.sendfile('intro6.html');
+})
+app.get('/hypertext',function(req,res){
+    res.sendfile('intro7.html');
+})
+
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!');
 });
